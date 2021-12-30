bab9d01f0fba6d7d481b94a1b517c16cec20dd9f
Jason Hills <jasonahills@gmail.com>
2021-12-30 15:35:16 -0500

    finishes d23

--- aoc2021/src/d23.rs
+++ aoc2021/src/d23.rs
@@ -39,6 +39,26 @@ impl Pawn {
       Self::D => 18,
     }
   }
+
+  pub fn expanded_room_exit_index(self) -> usize {
+    match self {
+      Self::A => 14,
+      Self::B => 18,
+      Self::C => 22,
+      Self::D => 26,
+    }
+  }
+
+  // lol this is terrible.
+  pub fn corresponding_expanded_entrance(u: usize) -> usize {
+    match u {
+      14 => 2,
+      18 => 4,
+      22 => 6,
+      26 => 8,
+      _ => panic!("asdf"),
+    }
+  }
 }
 
 type Dist = u32;
@@ -225,25 +245,286 @@ impl std::fmt::Display for Cavern {
   }
 }
 
-// #D#C#B#A#
-//   #D#B#A#C#
-// pub struct ExpandedCavern();
+#[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Hash, Copy, Clone)]
+pub struct ExpandedCavern([Option<Pawn>; 19 + 8]);
 
-// impl From<Cavern> for ExpandedCavern {
-//   fn from(c: Cavern) -> ExpandedCavern {
-//     let new = [0; 19 + 8];
-//     for i in 0..11 {
-//       if i < 11 {
-//         new[i] = c[i];
-//       }
-//     }
-//     for
-//     for i in 0..4 {
-//       new
-//     }
-//     Self()
-//   }
-// }
+impl ExpandedCavern {
+  pub fn goal() -> Self {
+    use Pawn::*;
+    Self([
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      None,
+      Some(A),
+      Some(A),
+      Some(A),
+      Some(A),
+      Some(B),
+      Some(B),
+      Some(B),
+      Some(B),
+      Some(C),
+      Some(C),
+      Some(C),
+      Some(C),
+      Some(D),
+      Some(D),
+      Some(D),
+      Some(D),
+    ])
+  }
+
+  pub fn mv(self, i1: usize, i2: usize) -> Self {
+    let mut new = self.0;
+    new[i1] = self.0[i2];
+    new[i2] = self.0[i1];
+    Self(new)
+  }
+
+  pub fn possible_moves(&self) -> Vec<(ExpandedCavern, Dist)> {
+    let mut moves = Vec::new();
+    for (i, v) in self.0.iter().enumerate() {
+      if let Some(p) = v {
+        // println!("i {:?} {:?}", i, p);
+        let cost = p.move_cost();
+        let in_hall = i <= 10;
+        // room enter
+        if in_hall {
+          let (open_path, spaces) = self.open_hall_path(i, p.room_entrance_index());
+          if open_path {
+            match self.0[p.expanded_room_exit_index() - 3..=p.expanded_room_exit_index()] {
+              [Some(p1), Some(p2), Some(p3), None] if *p == p1 && *p == p2 && *p == p3 => {
+                moves.push((
+                  self.mv(i, p.expanded_room_exit_index()),
+                  cost * (spaces + 1),
+                ));
+              }
+              [Some(p1), Some(p2), None, None] if *p == p1 && *p == p2 => {
+                moves.push((
+                  self.mv(i, p.expanded_room_exit_index() - 1),
+                  cost * (spaces + 2),
+                ));
+              }
+              [Some(p1), None, None, None] if *p == p1 => {
+                moves.push((
+                  self.mv(i, p.expanded_room_exit_index() - 2),
+                  cost * (spaces + 3),
+                ));
+              }
+              [None, None, None, None] => {
+                moves.push((
+                  self.mv(i, p.expanded_room_exit_index() - 3),
+                  cost * (spaces + 4),
+                ));
+              }
+              _ => (),
+            }
+          }
+        } else {
+          // exit near door
+          if i % 4 == 2 {
+            let entrance = Pawn::corresponding_expanded_entrance(i);
+            for j in [0, 1, 3, 5, 7, 9, 10] {
+              let (open_path, spaces) = self.open_hall_path_include_start(j, entrance);
+              if open_path {
+                moves.push((self.mv(i, j), cost * (spaces + 1)));
+              }
+            }
+          }
+          // exit from next back
+          if i % 4 == 1 && self.0[i + 1].is_none() {
+            let entrance = Pawn::corresponding_expanded_entrance(i + 1);
+            for j in [0, 1, 3, 5, 7, 9, 10] {
+              let (open_path, spaces) = self.open_hall_path_include_start(j, entrance);
+              if open_path {
+                moves.push((self.mv(i, j), cost * (spaces + 2)));
+              }
+            }
+          }
+
+          // exit from third
+          if i % 4 == 0 && self.0[i + 1].is_none() && self.0[i + 2].is_none() {
+            let entrance = Pawn::corresponding_expanded_entrance(i + 2);
+            for j in [0, 1, 3, 5, 7, 9, 10] {
+              let (open_path, spaces) = self.open_hall_path_include_start(j, entrance);
+              if open_path {
+                moves.push((self.mv(i, j), cost * (spaces + 3)));
+              }
+            }
+          }
+
+          // exit from fourth
+          if i % 4 == 3
+            && self.0[i + 1].is_none()
+            && self.0[i + 2].is_none()
+            && self.0[i + 3].is_none()
+          {
+            let entrance = Pawn::corresponding_expanded_entrance(i + 3);
+            for j in [0, 1, 3, 5, 7, 9, 10] {
+              let (open_path, spaces) = self.open_hall_path_include_start(j, entrance);
+              if open_path {
+                moves.push((self.mv(i, j), cost * (spaces + 4)));
+              }
+            }
+          }
+        }
+      }
+    }
+    moves
+  }
+
+  pub fn open_hall_path(&self, start: usize, end: usize) -> (bool, u32) {
+    if start < end {
+      (
+        self.0[start + 1..=end].iter().all(|e| e.is_none()),
+        (end - start) as u32,
+      )
+    } else {
+      (
+        self.0[end..=start - 1].iter().all(|e| e.is_none()),
+        (start - end) as u32,
+      )
+    }
+  }
+
+  pub fn open_hall_path_include_start(&self, start: usize, end: usize) -> (bool, u32) {
+    if start < end {
+      (
+        self.0[start..=end].iter().all(|e| e.is_none()),
+        (end - start) as u32,
+      )
+    } else {
+      (
+        self.0[end..=start].iter().all(|e| e.is_none()),
+        (start - end) as u32,
+      )
+    }
+  }
+
+  // TODO: refactor out of today and d15.
+  pub fn dijkstra(self) -> HashMap<ExpandedCavern, (u32, Option<ExpandedCavern>)> {
+    let goal = Self::goal();
+    // Start empty instead of with `unvisited` full so that we don't need to create unreachable states, if any
+    let mut visited = HashSet::new();
+    let mut entries = HashMap::new();
+    let mut distances = BinaryHeap::new();
+    entries.insert(self, (0, None));
+    let mut current = self;
+    loop {
+      if current.0[11..19].iter().filter(|i| i.is_some()).count() >= 7 {
+        // println!("current {:?} {}", visited.len(), current);
+      }
+      if visited.len() % 10000 == 0 {
+        // println!("visited {:?} {:?}", visited.len(), current);
+      }
+      visited.insert(current);
+      let current_dist = entries.get(&current).unwrap().0;
+      if current == goal {
+        return entries;
+      }
+      for (neighbor, delta) in current.possible_moves() {
+        if !visited.contains(&neighbor) {
+          let e = entries.entry(neighbor).or_insert((u32::MAX, None));
+          let proposed_dist = current_dist + delta;
+          if proposed_dist < e.0 {
+            *e = (proposed_dist, Some(current));
+            distances.push(Reverse((proposed_dist, neighbor)));
+          }
+        }
+      }
+
+      current = loop {
+        if let Some(Reverse((_, new_current))) = distances.pop() {
+          if !visited.contains(&new_current) {
+            break new_current;
+          }
+        } else {
+          return entries;
+        }
+      };
+    }
+  }
+}
+
+impl From<Cavern> for ExpandedCavern {
+  fn from(c: Cavern) -> ExpandedCavern {
+    let mut new = [None; 19 + 8];
+    for i in 0..11 {
+      new[i] = c.0[i];
+    }
+    new[11] = c.0[11];
+    new[12] = Some(Pawn::D);
+    new[13] = Some(Pawn::D);
+    new[14] = c.0[12];
+    new[15] = c.0[13];
+    new[16] = Some(Pawn::B);
+    new[17] = Some(Pawn::C);
+    new[18] = c.0[14];
+    new[19] = c.0[15];
+    new[20] = Some(Pawn::A);
+    new[21] = Some(Pawn::B);
+    new[22] = c.0[16];
+    new[23] = c.0[17];
+    new[24] = Some(Pawn::C);
+    new[25] = Some(Pawn::A);
+    new[26] = c.0[18];
+    Self(new)
+  }
+}
+
+impl std::fmt::Display for ExpandedCavern {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    let item = |x| match x {
+      Some(c) => format!("{:?}", c),
+      _ => ".".to_owned(),
+    };
+    write!(
+      f,
+      "#############
+#{}{}{}{}{}{}{}{}{}{}{}#
+###{}#{}#{}#{}###
+  #{}#{}#{}#{}#
+  #{}#{}#{}#{}#
+  #{}#{}#{}#{}#
+  #########",
+      item(self.0[0]),
+      item(self.0[1]),
+      item(self.0[2]),
+      item(self.0[3]),
+      item(self.0[4]),
+      item(self.0[5]),
+      item(self.0[6]),
+      item(self.0[7]),
+      item(self.0[8]),
+      item(self.0[9]),
+      item(self.0[10]),
+      item(self.0[14]),
+      item(self.0[18]),
+      item(self.0[22]),
+      item(self.0[26]),
+      item(self.0[13]),
+      item(self.0[17]),
+      item(self.0[21]),
+      item(self.0[25]),
+      item(self.0[12]),
+      item(self.0[16]),
+      item(self.0[20]),
+      item(self.0[24]),
+      item(self.0[11]),
+      item(self.0[15]),
+      item(self.0[19]),
+      item(self.0[23]),
+    )
+  }
+}
 
 type Input = Cavern;
 
@@ -295,16 +576,15 @@ pub fn parse(input: &str) -> IResult<&str, Input> {
 }
 
 pub fn p1(input: Input) -> u32 {
-  // let mut current = input.dijkstra().0.into_iter().last().unwrap().1 .1.unwrap();
-  // while let (dist, Some(c)) = input.dijkstra().0.get(&current).unwrap() {
-  //   println!("{} {}", dist, c);
-  //   current = *c;
-  // }
   input.dijkstra().0.get(&Cavern::goal()).unwrap().0
 }
 
-pub fn p2(_input: Input) -> usize {
-  0
+pub fn p2(input: Input) -> u32 {
+  ExpandedCavern::from(input)
+    .dijkstra()
+    .get(&ExpandedCavern::goal())
+    .unwrap()
+    .0
 }
 
 #[cfg(test)]
@@ -366,7 +646,7 @@ mod test {
   fn test_p2() {
     let input = TEST_INPUT;
     let parsed = parse(input).unwrap().1;
-    assert_eq!(p2(parsed), 0);
+    assert_eq!(p2(parsed), 44169);
 
     let input = std::fs::read_to_string("./inputs/d23.txt").unwrap();
     let parsed = parse(&input).unwrap().1;
--- aoc2021/src/d24.rs
+++ aoc2021/src/d24.rs
@@ -28,6 +28,7 @@ pub enum Inst {
 
 type Input = Vec<Inst>;
 
+#[derive(Debug, Eq, PartialEq)]
 pub struct Alu {
   w: i64,
   x: i64,
@@ -62,6 +63,9 @@ impl Alu {
           };
           self.write_register(r, val)
         }
+      }
+      if matches!(inst, Inst::Inp(_)) {
+        println!("inst {:?} alu {:?}", inst, self);
       }
     }
   }
@@ -126,31 +130,134 @@ pub fn parse(input: &str) -> IResult<&str, Input> {
   lines_of(inst)(input)
 }
 
+fn digits(mut n: i64) -> [i64; 14] {
+  let mut to_return = [0; 14];
+  for i in 0..14 {
+    let d = n % 10;
+    n /= 10;
+    to_return[13 - i] = d;
+  }
+  to_return
+}
+
 // Ok, the naïve approach isn't going to cut it here. Maybe I should get a way to prove particular instructions are unneeded.  Or maybe I express z in terms of the original inputs
-pub fn p1(insts: Input) -> u64 {
+pub fn p1(insts: Input) -> i64 {
+  // for my input, positions 3, 5, and 9 through 13 were div by 26 positions.
+
   // No need to left pad, as that would add a zero digit.
-  let mut n = 99999999999999_u64;
+  let mut n = 99999999999999;
+  // let n = 99989899988888_u64;
+  // let mut n = 99989899988888_u64;
+  // Assumingwe only need to play with the
+  // let n = 9999999_u64;
+
+  let start = std::time::Instant::now();
   loop {
-    let inputs: Vec<i64> = n
-      .to_string()
-      .split("")
-      .filter(|s| !s.is_empty())
-      .map(|d| d.parse::<i64>().unwrap())
-      .collect();
+    // let inputs: Vec<i64> = n
+    //   .to_string()
+    //   .split("")
+    //   .filter(|s| !s.is_empty())
+    //   .map(|d| d.parse::<i64>().unwrap())
+    //   .collect();
+
+    let inputs = digits(n);
+    // println!("digits {:?}", inputs);
+
+    n -= 1;
+
+    if n % 1_000_000 == 0 {
+      println!("{:?} {}", start.elapsed(), n);
+    }
+
     if inputs.iter().any(|v| *v == 0) {
       continue;
     }
-    let mut alu = Alu::new();
-    alu.run_program(&insts, inputs);
-    if alu.z == 0 {
-      return n;
+
+    // let mut alu = Alu::new();
+    // alu.run_program(&insts, inputs.clone());
+    // if alu.z == 0 {
+    //   return n;
+    // } else {
+    //   return 1;
+    // }
+    let inputs = inputs.clone().try_into().unwrap();
+    let out = my_program(inputs);
+    if out == 0 {
+      return n + 1;
     }
-    n -= 1;
+    // println!("my program output {} {} {:?}", out, alu.z, out == alu.z);
   }
+  // let inputs = "merrychristmas".split("").map(|c| {
+  //   'a' => 0,
+  //   'b'
+  //   'c'
+  //   'd'
+  //   'e'
+  //   'f'
+  //   'g'
+  //   'h'
+  //   'i'
+  //   'j'
+  // });
+  // let mut alu = Alu::new();
+  // alu.run_program(&insts, inputs);
 }
 
 pub fn p2(_input: Input) -> usize {
   0
+}
+
+pub fn bigger(w: i64, v1: i64, v2: i64, mut z: i64) -> i64 {
+  if w != (z % 26) + v1 {
+    z *= 26;
+  } else {
+    // println!("hit this");
+  }
+  z += v2 + w;
+  z
+}
+
+pub fn smaller(w: i64, v1: i64, v2: i64, mut z: i64) -> i64 {
+  z /= 26;
+  if w != (z % 26) + v1 {
+    z *= 26;
+  } else {
+    // println!("and hit this");
+  }
+  z += v2 + w;
+  z
+}
+
+pub fn my_program(xs: [i64; 14]) -> i64 {
+  let z = bigger(xs[0], 15, 15, 0);
+  // println!("{} {}", xs[0], z);
+  let z = bigger(xs[1], 12, 5, z);
+  // println!("{} {}", xs[1], z);
+  let z = bigger(xs[2], 13, 6, z);
+  // println!("{} {}", xs[2], z);
+  let z = smaller(xs[3], -14, 7, z);
+  // println!("{} {}", xs[3], z);
+  let z = bigger(xs[4], 15, 9, z);
+  // println!("{} {}", xs[4], z);
+  let z = smaller(xs[5], -7, 6, z);
+  // println!("{} {}", xs[5], z);
+  let z = bigger(xs[6], 14, 14, z);
+  // println!("{} {}", xs[6], z);
+  let z = bigger(xs[7], 15, 3, z);
+  // println!("{} {}", xs[7], z);
+  let z = bigger(xs[8], 15, 1, z);
+  // println!("{} {}", xs[8], z);
+  let z = smaller(xs[9], -7, 3, z);
+  // println!("{} {}", xs[9], z);
+  let z = smaller(xs[10], -8, 4, z);
+  // println!("{} {}", xs[10], z);
+  let z = smaller(xs[11], -7, 6, z);
+  // println!("{} {}", xs[11], z);
+  let z = smaller(xs[12], -5, 7, z);
+  // println!("{} {}", xs[12], z);
+  let z = smaller(xs[13], -10, 1, z);
+  // println!("{} {}", xs[12], z);
+  z
 }
 
 #[cfg(test)]
@@ -168,6 +275,17 @@ mod test {
   mod x 2
   div w 2
   mod w 2";
+
+  #[test]
+  fn test_bigger() {
+    for i in 1..=9 {
+      for j in 1..=9 {
+        let b = bigger(i, 15, 15, 0);
+        println!("{} {} {} {}", i, j, b, bigger(j, 12, 5, b));
+      }
+    }
+    panic!("asf");
+  }
 
   #[test]
   fn test_parse() {
@@ -210,6 +328,55 @@ mod test {
     let mut alu = Alu::new();
     alu.run_program(&insts, vec![3, 8]);
     assert_eq!(alu.z, 0);
+
+    let input = "inp w
+    add z w
+    mod z 2
+    div w 2
+    add y w
+    mod y 2
+    div w 2
+    add x w
+    mod x 2
+    div w 2
+    mod w 2";
+
+    let insts = parse(input).unwrap().1;
+    let mut alu = Alu::new();
+    alu.run_program(&insts, vec![9]);
+    assert_eq!(
+      alu,
+      Alu {
+        w: 1,
+        x: 0,
+        y: 0,
+        z: 1
+      }
+    );
+
+    let mut alu = Alu::new();
+    alu.run_program(&insts, vec![7]);
+    assert_eq!(
+      alu,
+      Alu {
+        w: 0,
+        x: 1,
+        y: 1,
+        z: 1
+      }
+    );
+
+    let mut alu = Alu::new();
+    alu.run_program(&insts, vec![5]);
+    assert_eq!(
+      alu,
+      Alu {
+        w: 0,
+        x: 1,
+        y: 0,
+        z: 1
+      }
+    );
   }
 
   #[test]
